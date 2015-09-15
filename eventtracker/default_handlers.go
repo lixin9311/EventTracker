49cc7f7eee66477fbb478bc74f08d25ea128ca52
lucus <lixin9311@gmail.com>
2015-09-15 18:02:58 +0800

    move the http handler code to eventtracker package.

---
+++ eventtracker/default_handlers.go
@@ -0,0 +1,236 @@
+package eventtracker
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type DefaultHandler struct {
+	logger *log.Logger
+	// MaxFileSize is the maximum size of upload file
+	MaxFileSize int64
+	// MaxMemorySize is the maximum memory size to handle the upload file
+	MaxMemorySize int64
+	fail_safe     *log.Logger
+	kafka         *Kafka
+	avro          *Avro
+}
+
+func NewDefaultHandler(w io.Writer, fail_safe *log.Logger, kafka *Kafka, avro *Avro) *DefaultHandler {
+	logger := log.New(w, "[http]", log.LstdFlags|log.Lshortfile)
+	return &DefaultHandler{logger: logger, MaxFileSize: int64(10 * 1024 * 1024), MaxMemorySize: int64(10 * 1024 * 1024), fail_safe: fail_safe, kafka: kafka, avro: avro}
+}
+
+func (self *DefaultHandler) PingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong")
+}
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong")
+}
+
+// HomeHandler is the index page for upload the csv file
+func (self *DefaultHandler) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		self.logger.Fatalln("Parse index template failed:", err)
+	}
+	t.Execute(w, nil)
+}
+
+// ErrorAndReturnCode prints an error and reponse to http client
+func (self *DefaultHandler) ErrorAndReturnCode(w http.ResponseWriter, errstr string, code int) {
+	self.logger.Println(errstr)
+	http.Error(w, errstr, code)
+}
+
+// UploadHandler handles the upload file
+func (self *DefaultHandler) UploadHandler(w http.ResponseWriter, r *http.Request) {
+	var remote string
+	if tmp := r.Header.Get("X-Forwarded-For"); tmp != "" {
+		remote = tmp
+	} else {
+		remote = r.RemoteAddr
+	}
+	self.logger.Println("Incomming upload file from:", remote)
+	// limit the file size
+	if r.ContentLength > self.MaxFileSize {
+		self.ErrorAndReturnCode(w, "The file is too large:"+strconv.FormatInt(r.ContentLength, 10)+"bytes", 400)
+		return
+	}
+	r.Body = http.MaxBytesReader(w, r.Body, self.MaxFileSize)
+	err := r.ParseMultipartForm(self.MaxMemorySize)
+	if err != nil {
+		self.ErrorAndReturnCode(w, "Failed to parse form:"+err.Error(), 500)
+		return
+	}
+	// get the file
+	file, _, err := r.FormFile("uploadfile")
+	if err != nil {
+		self.ErrorAndReturnCode(w, "Failed to read upload file:"+err.Error(), 500)
+		return
+	}
+	defer file.Close()
+	// read the file
+	csvreader := csv.NewReader(file)
+	record, err := csvreader.Read()
+	if err != nil {
+		self.ErrorAndReturnCode(w, "Failed to read the first line of file:"+err.Error(), 500)
+		return
+	}
+	title := map[string]int{}
+	ext := map[string]int{}
+	extension := map[string](interface{}){}
+	// read title and extensions
+	for k, v := range record {
+		if v == "did" || v == "aid" || v == "ip" || v == "timestamp" {
+			title[v] = k
+		} else {
+			ext[v] = k
+		}
+	}
+	if _, ok := title["did"]; !ok {
+		self.ErrorAndReturnCode(w, "Missing Required field: No did", 400)
+		return
+	}
+	if _, ok := title["timestamp"]; !ok {
+		self.ErrorAndReturnCode(w, "Missing Required field: No timestamp", 400)
+		return
+	}
+	if _, ok := ext["event_type"]; !ok {
+		self.ErrorAndReturnCode(w, "Missing Required field: No event_type", 400)
+		return
+	}
+	// read the record one by one and send it to kafka
+	buf := new(bytes.Buffer)
+	counter := 0
+	for {
+		// one more line
+		record, err := csvreader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			self.ErrorAndReturnCode(w, "Err read file:"+err.Error(), 500)
+			return
+		}
+		arecord, err := self.avro.NewRecord()
+		if err != nil {
+			self.ErrorAndReturnCode(w, "Failed to set new avro record:"+err.Error(), 500)
+			return
+		}
+		// set main title
+		for k, v := range title {
+			arecord.Set(k, record[v])
+		}
+		// set ext map
+		for k, v := range ext {
+			extension[k] = record[v]
+		}
+		if len(extension) != 0 {
+			arecord.Set("extension", extension)
+		}
+		// fullfill the event.avsc required fields
+		arecord.Set("event", "TrackerEvent")
+		arecord.Set("id", "")
+		// encode avro
+		if err = self.avro.Encode(buf, arecord); err != nil {
+			self.ErrorAndReturnCode(w, "Failed to encode avro record:"+err.Error(), 500)
+			return
+		}
+		// send to kafka
+		_, _, err = self.kafka.SendByteMessage(buf.Bytes(), record[ext["event_type"]])
+		if err != nil {
+			self.fail_safe.Println("error:", err)
+			self.fail_safe.Println("record:", arecord)
+			self.fail_safe.Println("data:", buf.Bytes())
+			self.ErrorAndReturnCode(w, "Failed to send to kafka:"+err.Error(), 500)
+			return
+		}
+		buf.Reset()
+		counter++
+	}
+	// done
+	self.logger.Printf("%d messages have been writen.", counter)
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%d messages have been writen.", counter)
+}
+
+// EventHandler is the REST api handler
+func (self *DefaultHandler) EventHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	var remote string
+	if tmp := r.Header.Get("X-Forwarded-For"); tmp != "" {
+		remote = tmp
+	} else {
+		remote = r.RemoteAddr
+	}
+	self.logger.Println("Incomming event from:", remote)
+	// required fields
+	if len(r.Form["did"]) < 1 {
+		self.ErrorAndReturnCode(w, "Missing Required field: No did", 400)
+		return
+	}
+	if len(r.Form["timestamp"]) < 1 {
+		self.ErrorAndReturnCode(w, "Missing Required field: No timestamp", 400)
+		return
+	}
+	if len(r.Form["event_type"]) < 1 {
+		self.ErrorAndReturnCode(w, "Missing Required field: No event_type", 400)
+		return
+	}
+	// set a new avro record
+	record, err := self.avro.NewRecord()
+	if err != nil {
+		self.ErrorAndReturnCode(w, "Failed to set a new avro record:"+err.Error(), 500)
+		return
+	}
+	// optional fields
+	if len(r.Form["ip"]) > 0 {
+		record.Set("ip", r.Form["ip"][0])
+	}
+	if len(r.Form["aid"]) > 0 {
+		record.Set("aid", r.Form["aid"][0])
+	}
+	// set required fields
+	record.Set("did", r.Form["did"][0])
+	record.Set("timestamp", r.Form["timestamp"][0])
+	record.Set("event", "TrackerEvent")
+	record.Set("id", "")
+	// extensions fields
+	extension := map[string](interface{}){}
+	for k, v := range r.Form {
+		if k != "ip" && k != "aid" && k != "did" && k != "timestamp" {
+			extension[k] = v[0]
+		}
+	}
+	if len(extension) != 0 {
+		record.Set("extension", extension)
+	}
+	// encode avro
+	buf := new(bytes.Buffer)
+	if err = self.avro.Encode(buf, record); err != nil {
+		self.logger.Println("AVRO record:", record)
+		self.ErrorAndReturnCode(w, "Failed to encode avro record:"+err.Error(), 500)
+		return
+	}
+	// send to kafka
+	part, offset, err := self.kafka.SendByteMessage(buf.Bytes(), r.Form["event_type"][0])
+	if err != nil {
+		self.fail_safe.Println("error:", err)
+		self.fail_safe.Println("record:", record)
+		self.fail_safe.Println("data:", buf.Bytes())
+		self.ErrorAndReturnCode(w, "Failed to send message to kafka:"+err.Error()+"Data has been writen to a backup file. Please contact us.", 500)
+		return
+	}
+	// done
+	self.logger.Printf("New record partition=%d\toffset=%d\n", part, offset)
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "1 messages have been writen.")
+}
--- main.go
+++ main.go
@@ -1,254 +1,38 @@
 package main
 
 import (
-	"bytes"
-	"encoding/csv"
 	"flag"
-	"fmt"
 	"github.com/gorilla/mux"
 	et "github.com/lixin9311/EventTracker/eventtracker"
-	"html/template"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
-	"strconv"
 )
 
 var (
-	// MaxFileSize is the maximum size of upload file
-	MaxFileSize = int64(10 * 1024 * 1024)
-	// MaxMemorySize is the maximum memory size to handle the upload file
-	MaxMemorySize = int64(10 * 1024 * 1024)
-	logger        *log.Logger
-	conf          *et.Config
-	configFile    = flag.String("c", "config.json", "Config file in json.")
+	logger     *log.Logger
+	conf       *et.Config
+	configFile = flag.String("c", "config.json", "Config file in json.")
 	// Fail safe buffer file
-	fail_safe *log.Logger
-	address   []string
-	kafka     *et.Kafka
-	avro      *et.Avro
+	address        []string
+	defaultHandler *et.DefaultHandler
+	kafka          *et.Kafka
 )
-
-// HomeHandler is the index page for upload the csv file
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
-	if err != nil {
-		logger.Fatalln("Parse index template failed:", err)
-	}
-	t.Execute(w, nil)
-}
-
-// ErrorAndReturnCode prints an error and reponse to http client
-func ErrorAndReturnCode(w http.ResponseWriter, errstr string, code int) {
-	logger.Println(errstr)
-	http.Error(w, errstr, code)
-}
-
-func PingHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Pong")
-}
-
-// UploadHandler handles the upload file
-func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	var remote string
-	if tmp := r.Header.Get("X-Forwarded-For"); tmp != "" {
-		remote = tmp
-	} else {
-		remote = r.RemoteAddr
-	}
-	logger.Println("Incomming upload file from:", remote)
-	// limit the file size
-	if r.ContentLength > MaxFileSize {
-		ErrorAndReturnCode(w, "The file is too large:"+strconv.FormatInt(r.ContentLength, 10)+"bytes", 400)
-		return
-	}
-	r.Body = http.MaxBytesReader(w, r.Body, MaxFileSize)
-	err := r.ParseMultipartForm(MaxMemorySize)
-	MaxFileSize = 10 * 1024 * 1024
-	if err != nil {
-		ErrorAndReturnCode(w, "Failed to parse form:"+err.Error(), 500)
-		return
-	}
-	// get the file
-	file, _, err := r.FormFile("uploadfile")
-	if err != nil {
-		ErrorAndReturnCode(w, "Failed to read upload file:"+err.Error(), 500)
-		return
-	}
-	defer file.Close()
-	// read the file
-	csvreader := csv.NewReader(file)
-	record, err := csvreader.Read()
-	if err != nil {
-		ErrorAndReturnCode(w, "Failed to read the first line of file:"+err.Error(), 500)
-		return
-	}
-	title := map[string]int{}
-	ext := map[string]int{}
-	extension := map[string](interface{}){}
-	// read title and extensions
-	for k, v := range record {
-		if v == "did" || v == "aid" || v == "ip" || v == "timestamp" {
-			title[v] = k
-		} else {
-			ext[v] = k
-			logger.Println("csv file extension title:", v)
-		}
-	}
-	if _, ok := title["did"]; !ok {
-		ErrorAndReturnCode(w, "Missing Required field: No did", 400)
-		return
-	}
-	if _, ok := title["timestamp"]; !ok {
-		ErrorAndReturnCode(w, "Missing Required field: No timestamp", 400)
-		return
-	}
-	if _, ok := ext["event_type"]; !ok {
-		ErrorAndReturnCode(w, "Missing Required field: No event_type", 400)
-		return
-	}
-	// read the record one by one and send it to kafka
-	buf := new(bytes.Buffer)
-	counter := 0
-	for {
-		// one more line
-		record, err := csvreader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			ErrorAndReturnCode(w, "Err read file:"+err.Error(), 500)
-			return
-		}
-		arecord, err := avro.NewRecord()
-		if err != nil {
-			ErrorAndReturnCode(w, "Failed to set new avro record:"+err.Error(), 500)
-			return
-		}
-		// set main title
-		for k, v := range title {
-			arecord.Set(k, record[v])
-		}
-		// set ext map
-		for k, v := range ext {
-			extension[k] = record[v]
-		}
-		if len(extension) != 0 {
-			arecord.Set("extension", extension)
-		}
-		// fullfill the event.avsc required fields
-		arecord.Set("event", "TrackerEvent")
-		arecord.Set("id", "")
-		// encode avro
-		if err = avro.Encode(buf, arecord); err != nil {
-			ErrorAndReturnCode(w, "Failed to encode avro record:"+err.Error(), 500)
-			return
-		}
-		// send to kafka
-		_, _, err = kafka.SendByteMessage(buf.Bytes(), record[ext["event_type"]])
-		if err != nil {
-			fail_safe.Println("error:", err)
-			fail_safe.Println("record:", arecord)
-			fail_safe.Println("data:", buf.Bytes())
-			ErrorAndReturnCode(w, "Failed to send to kafka:"+err.Error(), 500)
-			return
-		}
-		buf.Reset()
-		counter++
-	}
-	// done
-	logger.Printf("%d messages have been writen.", counter)
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "%d messages have been writen.", counter)
-}
-
-// EventHandler is the REST api handler
-func EventHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	var remote string
-	if tmp := r.Header.Get("X-Forwarded-For"); tmp != "" {
-		remote = tmp
-	} else {
-		remote = r.RemoteAddr
-	}
-	logger.Println("Incomming event from:", remote)
-	// required fields
-	if len(r.Form["did"]) < 1 {
-		ErrorAndReturnCode(w, "Missing Required field: No did", 400)
-		return
-	}
-	if len(r.Form["timestamp"]) < 1 {
-		ErrorAndReturnCode(w, "Missing Required field: No timestamp", 400)
-		return
-	}
-	if len(r.Form["event_type"]) < 1 {
-		ErrorAndReturnCode(w, "Missing Required field: No event_type", 400)
-		return
-	}
-	// set a new avro record
-	record, err := avro.NewRecord()
-	if err != nil {
-		ErrorAndReturnCode(w, "Failed to set a new avro record:"+err.Error(), 500)
-		return
-	}
-	// optional fields
-	if len(r.Form["ip"]) > 0 {
-		record.Set("ip", r.Form["ip"][0])
-	}
-	if len(r.Form["aid"]) > 0 {
-		record.Set("aid", r.Form["aid"][0])
-	}
-	// set required fields
-	record.Set("did", r.Form["did"][0])
-	record.Set("timestamp", r.Form["timestamp"][0])
-	record.Set("event", "TrackerEvent")
-	record.Set("id", "")
-	// extensions fields
-	extension := map[string](interface{}){}
-	for k, v := range r.Form {
-		if k != "ip" && k != "aid" && k != "did" && k != "timestamp" {
-			extension[k] = v[0]
-		}
-	}
-	if len(extension) != 0 {
-		record.Set("extension", extension)
-	}
-	// encode avro
-	buf := new(bytes.Buffer)
-	if err = avro.Encode(buf, record); err != nil {
-		logger.Println("AVRO record:", record)
-		ErrorAndReturnCode(w, "Failed to encode avro record:"+err.Error(), 500)
-		return
-	}
-	// send to kafka
-	part, offset, err := kafka.SendByteMessage(buf.Bytes(), r.Form["event_type"][0])
-	if err != nil {
-		fail_safe.Println("error:", err)
-		fail_safe.Println("record:", record)
-		fail_safe.Println("data:", buf.Bytes())
-		ErrorAndReturnCode(w, "Failed to send message to kafka:"+err.Error()+"Data has been writen to a backup file. Please contact us.", 500)
-		return
-	}
-	// done
-	logger.Printf("New record partition=%d\toffset=%d\n", part, offset)
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "1 messages have been writen.")
-}
 
 func init() {
 	flag.Parse()
 	// open config
 	conf = et.ParseConfig(*configFile)
 	logger = log.New(os.Stderr, "[main]:", log.LstdFlags|log.Lshortfile)
-	// parse flags and override the config file
+	// parVse flags and override the config file
 	safe_file, err := os.OpenFile(conf.Main.Backup_file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		logger.Fatalln("Failed to open backup file:", err)
 	}
-	fail_safe = log.New(safe_file, "", log.LstdFlags)
+	fail_safe := log.New(safe_file, "", log.LstdFlags)
 	file, err := os.OpenFile(conf.Main.Log_file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		logger.Fatalln("Failed to open log file:", err)
@@ -258,9 +42,10 @@ func init() {
 	logger.SetOutput(w)
 	logger.Println("========== Loading Config Complete ==========")
 	// init avro
-	avro = et.NewAvroInst(w, conf.Avro)
+	avro := et.NewAvroInst(w, conf.Avro)
 	// init kafka
 	kafka = et.NewKafkaInst(w, conf.Kafka)
+	defaultHandler = et.NewDefaultHandler(w, fail_safe, kafka, avro)
 }
 
 func main() {
@@ -270,10 +55,10 @@ func main() {
 	defer logger.Println("Instance down.")
 	// REST route
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/event", EventHandler)
-	r.HandleFunc("/upload", UploadHandler)
-	r.HandleFunc("/ping", PingHandler)
+	r.HandleFunc("/", defaultHandler.HomeHandler)
+	r.HandleFunc("/event", defaultHandler.EventHandler)
+	r.HandleFunc("/upload", defaultHandler.UploadHandler)
+	r.HandleFunc("/ping", et.PingHandler)
 	// bring up the service
 	var ln net.Listener
 	if conf.Front.Enabled == true {
